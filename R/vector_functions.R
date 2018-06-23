a1037d1a8ba213b5a78e2f2e9ab74fbb12cc3280
welch6 <welch16@gmail.com>
2018-06-22 20:25:30 -0500

    added function to remove_names of a vector

---
+++ R/vector_functions.R
@@ -0,0 +1,15 @@
+##' Removes the names of the vector
+##'
+##' \code{remove_names} is a function to remove the names of a vector.
+##'
+##' @param x Vector to remove names
+##'
+##' @example
+##' x = c("a" = 1,"b" = 3)
+##' remove_names(x)
+##'
+remove_names <- function(x)
+{
+  names(x) = NULL
+  x
+}
